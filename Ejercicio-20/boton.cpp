7b1ec0573e0a6357dbefec22caae51bd78069668
Mercado Morena Valentina <163059849+MValentinaMercado@users.noreply.github.com>
2024-05-16 14:44:30 -0300

    Add files via upload

---
+++ Ejercicio-20/boton.cpp
@@ -0,0 +1,65 @@
+#include "boton.h"
+#include "ui_boton.h"
+#include <QPainter>
+#include <QColor>
+
+Boton::Boton( QWidget * parent ) : QWidget( parent ),
+                                   ui( new Ui::Boton ),
+                                   color( BLANCO )
+{
+    ui->setupUi(this);
+}
+
+Boton::~Boton()
+{
+    delete ui;
+}
+
+void Boton::setTexto(QString texto)
+{
+    ui->lTexto->setText( texto );
+}
+
+void Boton::setColor( Color color )
+{
+    this->color = color;
+    this->repaint();
+}
+
+void Boton::mousePressEvent(QMouseEvent *e)
+{
+    if ( e->button() == Qt::LeftButton )  {
+        emit signal_click();
+    }
+}
+
+void Boton::paintEvent(QPaintEvent *)
+{
+    QPainter painter(this);
+
+    switch (color) {
+    case BLANCO:
+        painter.fillRect(0, 0, this->width(), this->height(), QColor(255, 255, 255,60));
+        break;
+    case ROJO:
+        painter.fillRect(0, 0, this->width(), this->height(), QColor(255, 0, 0,60));
+        break;
+    case MAGENTA:
+        painter.fillRect(0, 0, this->width(), this->height(), QColor(255, 0, 255,60));
+        break;
+    case MORADO:
+        painter.fillRect(0, 0, this->width(), this->height(), QColor(128, 0, 128,60));
+        break;
+    case AZUL:
+        painter.fillRect(0, 0, this->width(), this->height(), QColor(0, 0, 255,60));
+        break;
+    case VERDE:
+        painter.fillRect(0, 0, this->width(), this->height(), QColor(0, 255, 0,60));
+        break;
+    default:
+        painter.fillRect(0, 0, this->width(), this->height(), QColor(255, 255, 255,60));
+        break;
+    }
+}
+
+
---
+++ Ejercicio-20/boton.h
@@ -0,0 +1,35 @@
+#ifndef BOTON_H
+#define BOTON_H
+
+#include <QWidget>
+#include <QMouseEvent>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Boton; }
+QT_END_NAMESPACE
+
+class Boton : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Boton(QWidget *parent = nullptr);
+    ~Boton();
+
+    enum Color { BLANCO, ROJO, MAGENTA, MORADO, AZUL, VERDE };
+
+    void setTexto( QString texto );
+    void setColor( Color color );
+
+protected:
+     void mousePressEvent( QMouseEvent * event );
+     void paintEvent( QPaintEvent * );
+
+private:
+    Ui::Boton *ui;
+    Color color;
+
+signals:
+    void signal_click();
+};
+#endif // BOTON_H
